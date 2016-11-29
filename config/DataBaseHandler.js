961eac5d51e743f5a53d3e94d85915155f582cc7
Carlos Leonardo Camilo Vargas Huamnan <carlosleonardovargashuaman@gmail.com>
2016-11-28 23:39:52 -0500

    wip(DataBaseHandler): I've added a new connection file

---
+++ config/DataBaseHandler.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Carlos Leonardo Camilo Vargas HUuamán on 11/20/16.
+ */
+
+var mysql = require("mysql");
+
+function DataBaseHandler() {
+    this.connection = null;
+}
+
+DataBaseHandler.prototype.createConnection = function () {
+
+    this.connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "android123",
+        database: "ERP_CHIRI"
+    });
+
+    this.connection.connect(function (err) {
+        if (err) {
+            console.error("error connecting " + err.stack);
+            return null;
+        }
+        console.log("connected as id " + this.threadId);
+    });
+    return this.connection;
+};
+
+module.exports = DataBaseHandler;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var DataBaseHandler = require("../config/DataBaseHandler");
+var dataBaseHandler = new DataBaseHandler();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var connection = dataBaseHandler.createConnection();
+
+router.get('/', function (req, res, next) {
+  connection.query("SELECT * FROM CUSTOMER", function (err, rows, fields) {
+    if(err) throw err;
+    res.status(200).send(rows);
+  });
+});
+
+router.post('/endpoint', function(req, res){
+  var obj = {};
+  console.log('body: ' + JSON.stringify(req.body));
+  res.send(req.body);
 });
 
 module.exports = router;
